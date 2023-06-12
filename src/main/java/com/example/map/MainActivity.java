27c61562aa126b5d68e657ef71bbae42d1deeefb
jeroldmanansala <123334939+jeroldmanansala@users.noreply.github.com>
2023-06-12 12:09:38 -0700

    Add files via upload

---
+++ src/main/java/com/example/map/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fragment fragment = new Map_Fragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,fragment).commit();
+
+    }
+}
